54f4c49aef654312e5441d46149921ce785a82bf
LoopMaster99 <bansalkrishna909@gmail.com>
2025-11-03 02:39:41 +0530

    added greeting endpoint in UserController to return user name with current weather info

--- src/main/java/com/developmentprep/journalApp/controller/UserController.java
+++ src/main/java/com/developmentprep/journalApp/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.developmentprep.journalApp.controller;
 
+import com.developmentprep.journalApp.api.response.WeatherResponse;
 import com.developmentprep.journalApp.entity.User;
 import com.developmentprep.journalApp.repository.UserRepository;
 import com.developmentprep.journalApp.service.UserService;
+import com.developmentprep.journalApp.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +21,9 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private WeatherService weatherService;
 
      @PutMapping
      public ResponseEntity<?> updateUser(@RequestBody User user){
@@ -39,6 +44,17 @@ public class UserController {
          Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
          userRepository.deleteByUserName(authentication.getName());
          return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+     }
+
+     @GetMapping
+     public ResponseEntity<?> greeting(){
+         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+         WeatherResponse weatherResponse = weatherService.getWeather("Noida");
+         String greeting = "";
+         if(weatherResponse != null){
+             greeting = ", Weather feels like " + weatherResponse.getCurrent().getFeelslike();
+         }
+         return new ResponseEntity<>("Hi " + authentication.getName() + greeting, HttpStatus.OK);
      }
 
 }
