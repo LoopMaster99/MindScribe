42397c4bf8d5b3ee377d28b1d6905d9cee286397
LoopMaster99 <bansalkrishna909@gmail.com>
2025-11-07 16:34:33 +0530

    added email and sentimentAnalysis fields to User entity

--- src/main/java/com/developmentprep/journalApp/entity/User.java
+++ src/main/java/com/developmentprep/journalApp/entity/User.java
@@ -21,6 +21,10 @@ public class User {
     @Indexed(unique = true)
     @NonNull
     private String userName;
+
+    private String email;
+    private boolean sentimentAnalysis;
+
     @NonNull
     private String password;
 
