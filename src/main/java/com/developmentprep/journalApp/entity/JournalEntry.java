fde6a568f5c21317e22c02efb46b0d5e77122f67
LoopMaster99 <bansalkrishna909@gmail.com>
2025-11-07 16:43:50 +0530

    added sentiment field to JournalEntry entity

--- src/main/java/com/developmentprep/journalApp/entity/JournalEntry.java
+++ src/main/java/com/developmentprep/journalApp/entity/JournalEntry.java
@@ -1,5 +1,6 @@
 package com.developmentprep.journalApp.entity;
 
+import com.developmentprep.journalApp.enums.Sentiment;
 import lombok.*;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
@@ -17,4 +18,5 @@ public class JournalEntry {
     private String title;
     private String content;
     private LocalDateTime date;
+    private Sentiment sentiment;
 }
