539a89f4acd96e28377449de29d79532bdbbea4f
LoopMaster99 <bansalkrishna909@gmail.com>
2025-12-15 03:53:01 +0530

    feat: add custom query methods to JournalEntryRepository

--- src/main/java/com/developmentprep/journalApp/repository/JournalEntryRepository.java
+++ src/main/java/com/developmentprep/journalApp/repository/JournalEntryRepository.java
@@ -1,9 +1,18 @@
 package com.developmentprep.journalApp.repository;
 
 import com.developmentprep.journalApp.entity.JournalEntry;
+import com.developmentprep.journalApp.enums.Sentiment;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
 
+    List<JournalEntry> findByDateBetween(LocalDateTime start, LocalDateTime end);
+
+    List<JournalEntry> findBySentiment(Sentiment sentiment);
+
+    long countBySentiment(Sentiment sentiment);
 }
