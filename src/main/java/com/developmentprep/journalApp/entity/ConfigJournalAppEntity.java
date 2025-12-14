4e7fea6f04d166b6e930499c4f158458f8729fd0
LoopMaster99 <bansalkrishna909@gmail.com>
2025-12-15 03:41:41 +0530

    feat: add @Id field to ConfigJournalAppEntity

--- src/main/java/com/developmentprep/journalApp/entity/ConfigJournalAppEntity.java
+++ src/main/java/com/developmentprep/journalApp/entity/ConfigJournalAppEntity.java
@@ -2,6 +2,9 @@ package com.developmentprep.journalApp.entity;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "config_journal_app")
@@ -9,6 +12,10 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @NoArgsConstructor
 public class ConfigJournalAppEntity {
 
+    @Id
+    private ObjectId id;
+
+    @Indexed(unique = true)
     private String key;
     private String value;
 
