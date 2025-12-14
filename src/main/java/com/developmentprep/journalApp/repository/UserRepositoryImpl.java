179494cbe1fc3e63a713be4e6d3adf2a03d42c9b
LoopMaster99 <bansalkrishna909@gmail.com>
2025-12-15 03:51:31 +0530

    refactor: add @Repository and constructor injection to UserRepositoryImpl

--- src/main/java/com/developmentprep/journalApp/repository/UserRepositoryImpl.java
+++ src/main/java/com/developmentprep/journalApp/repository/UserRepositoryImpl.java
@@ -1,23 +1,27 @@
 package com.developmentprep.journalApp.repository;
 
 import com.developmentprep.journalApp.entity.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class UserRepositoryImpl {
 
-    @Autowired
-    private MongoTemplate mongoTemplate;
+    private final MongoTemplate mongoTemplate;
 
-    public List<User> getUsersForSA(){
+    public UserRepositoryImpl(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    public List<User> getUsersForSA() {
         Query query = new Query();
-        query.addCriteria(Criteria.where("email").regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"));
+        query.addCriteria(Criteria.where("email").exists(true).ne(""));
+        query.addCriteria(Criteria.where("email").regex("^.+@.+\\..+$"));
         query.addCriteria(Criteria.where("sentimentAnalysis").is(true));
-        List<User> users = mongoTemplate.find(query, User.class);
-        return users;
+        return mongoTemplate.find(query, User.class);
     }
 }
